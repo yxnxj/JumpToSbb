19fdeb316f9807f502dbff06215c271c1f6e8191
YeonJun Cho <micayel1219@gmail.com>
2022-08-09 15:39:58 +0900

    생성자가 여러개라면,인자가 없는 생성자 사용

--- src/main/java/com/ll/exam/sbb/Person.java
+++ src/main/java/com/ll/exam/sbb/Person.java
@@ -1,14 +1,24 @@
 package com.ll.exam.sbb;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     static Integer lastId = 0;
 
     private int id;
     private int age;
     private String name;
+
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 }
