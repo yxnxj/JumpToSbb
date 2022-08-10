47e9d6005b716e677895436c0f47c4f99dae4317
YeonJun Cho <micayel1219@gmail.com>
2022-08-10 13:54:57 +0900

    jpa repository save test case add

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -1,13 +1,31 @@
 package com.ll.exam.sbb;
 
+import com.ll.exam.sbb.model.Question;
+import com.ll.exam.sbb.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
-class SbbApplicationTests {
+public class SbbApplicationTests {
+    @Autowired
+    private QuestionRepository questionRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testJpa(){
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
 
+        Question q2 = new Question();
+        q2.setSubject("스프링 부트 모델 질문입니다.");
+        q2.setContent("id는 자동 생성 되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+
+        questionRepository.save(q2);
+    }
 }
