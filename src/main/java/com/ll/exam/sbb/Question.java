f05cf27dd8355c77c7ef46ad5a5b14ded368a58f
YeonJun Cho <micayel1219@gmail.com>
2022-08-10 13:05:32 +0900

    Question Entity add

---
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -0,0 +1,22 @@
+package com.ll.exam.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity // 아래 Question 클래스는 엔티티 클래스이다.
+// 아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
+public class Question {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+    @Column(length = 200) // varchar(200)
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime createDate;
+}
