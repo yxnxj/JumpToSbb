fa9d283f75343b5ec116283e21102be7d974635e
YeonJun Cho <micayel1219@gmail.com>
2022-08-18 13:52:09 +0900

    user create test

---
+++ src/main/java/com/ll/exam/sbb/user/UserService.java
@@ -0,0 +1,22 @@
+package com.ll.exam.sbb.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
