87d3fd2ab0a077a0d4fdfdb15a47dde3ebe36db4
YeonJun Cho <micayel1219@gmail.com>
2022-08-18 13:45:33 +0900

    h2콘솔 시큐리티 작동 안하도록

--- src/main/java/com/ll/exam/sbb/SecurityConfig.java
+++ src/main/java/com/ll/exam/sbb/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 // 시큐리티 설정
 @Configuration
@@ -14,10 +15,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                // 모든 경로
-                .antMatchers("/**")
-                // 허용 한다.
-                .permitAll();
+                .antMatchers("/**").permitAll()
+                .and() // 문맥의 끝
+                .csrf().ignoringAntMatchers("/h2-console/**")
+                .and()
+                .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
         return http.build();
     }
 }
