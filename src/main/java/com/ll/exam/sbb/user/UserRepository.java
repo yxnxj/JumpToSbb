405f4eec0220430774b8412bf8d3ba6f62478ed0
YeonJun Cho <micayel1219@gmail.com>
2022-08-19 14:43:39 +0900

    회원관련 테스트케이스를 실행하기 전, 답변, 질문, 회원 순으로 삭제, 회원 2명 생성 후 진행

--- src/main/java/com/ll/exam/sbb/user/UserRepository.java
+++ src/main/java/com/ll/exam/sbb/user/UserRepository.java
@@ -1,11 +1,20 @@
 package com.ll.exam.sbb.user;
 
+import com.ll.exam.sbb.base.RepositoryUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<SiteUser, Long> {
+public interface UserRepository extends JpaRepository<SiteUser, Long>, RepositoryUtil {
     boolean existsByUsername(String username);
 
     Optional<SiteUser> findByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query(value = "ALTER TABLE site_user AUTO_INCREMENT = 1", nativeQuery = true)
+    void truncate(); // 이거 지우면 안됨, truncateTable 하면 자동으로 이게 실행됨
 }
