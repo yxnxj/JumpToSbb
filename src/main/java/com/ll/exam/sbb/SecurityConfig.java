abff8dd7abee8cd3403657b7d818d38cbf35eb94
YeonJun Cho <micayel1219@gmail.com>
2022-08-19 11:26:46 +0900

    로그인 시큐리티 처리

--- src/main/java/com/ll/exam/sbb/SecurityConfig.java
+++ src/main/java/com/ll/exam/sbb/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.ll.exam.sbb;
 
 
+import com.ll.exam.sbb.user.UserSecurityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +17,9 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 // 시큐리티 설정
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserSecurityService userSecurityService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -33,5 +39,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
