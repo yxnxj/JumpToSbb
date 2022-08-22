fc521b5b2f06b82ce05e2e9329846ced17df6d3e
YeonJun Cho <micayel1219@gmail.com>
2022-08-22 13:57:19 +0900

    작성자 리스트에 표시

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -159,7 +159,7 @@ public class QuestionRepositoryTests {
             Question q = new Question();
             q.setSubject("%d번 질문".formatted(id));
             q.setContent("%d번 질문의 내용".formatted(id));
-            q.setAuthor(new SiteUser(2L));
+            q.setAuthor(new SiteUser(2l));
             q.setCreateDate(LocalDateTime.now());
             questionRepository.save(q);
         });
