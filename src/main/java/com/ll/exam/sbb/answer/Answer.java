2d0323682a7e4ee9398b8153b66f944f721ec1ca
YeonJun Cho <micayel1219@gmail.com>
2022-08-23 14:37:52 +0900

    질문과 답변에 추천 테이블 구현

--- src/main/java/com/ll/exam/sbb/answer/Answer.java
+++ src/main/java/com/ll/exam/sbb/answer/Answer.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -26,4 +27,7 @@ public class Answer {
 
     @ManyToOne
     private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
--- src/main/java/com/ll/exam/sbb/question/Question.java
+++ src/main/java/com/ll/exam/sbb/question/Question.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -36,4 +37,6 @@ public class Question {
     @ManyToOne
     private SiteUser author;
 
+    @ManyToMany
+    Set<SiteUser> voter;
 }
