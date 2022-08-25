14519fe27b8f8f256de63c325280b0ed7813d4d0
YeonJun Jo <micayel1219@gmail.com>
2022-08-25 09:20:21 +0900

    kw 파라미터가 질문제목 + 질문내용 + 질문작성자명 + 답변내용 검색어로 작동

--- src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
@@ -28,4 +28,6 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, Repos
 
     Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContains(String kw, String kw_, String kw__, Pageable pageable);
 
+    Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
+
 }
